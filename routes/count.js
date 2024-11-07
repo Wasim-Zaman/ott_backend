ef60b84f8d35ff0c72e612106fe4d647305d0b75
Wasim Zaman <wasimxaman13@gmail.com>
2024-11-07 09:01:40 +0500

    Refactor codebase and remove unused files and routes

--- routes/count.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getCounts } = require("../controllers/count");
-const verifyToken = require("../middleware/isAdmin");
-
-router.get("/", verifyToken, getCounts);
-
-module.exports = router;
